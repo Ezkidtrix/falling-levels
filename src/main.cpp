7e43fa1fa2f65d930c95de3399eaae8835523c03
BuildTools <unconfigured@null.spigotmc.org>
2026-08-06 16:05:00 -0700

    Add base files

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(GJBaseGameLayer) {
+  void update(float dt) {
+    GJBaseGameLayer::update(dt);
+
+    
+  }
+};
