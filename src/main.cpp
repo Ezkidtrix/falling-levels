96253543c3a14c4a1f40de68c8dd72d267a31bcd
BuildTools <unconfigured@null.spigotmc.org>
2026-08-06 16:30:29 -0700

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/loader/Mod.hpp>
+#include <math.h>
 
 using namespace geode::prelude;
+
+struct Settings {
+  bool enabled = true;
+
+  float fallSpeed = 0.5;
+  float fallOffset = 50.0;
+};
+static Settings settings;
 
 class $modify(GJBaseGameLayer) {
   void update(float dt) {
     GJBaseGameLayer::update(dt);
+    if (!settings.enabled) return;
 
     CCArray* objects = this->m_objects;
     if (!objects) return;
@@ -20,7 +31,29 @@ class $modify(GJBaseGameLayer) {
       if (!obj) continue;
 
       CCPoint objPos = obj->getPosition();
-      
+      float distance = std::abs(std::min(0.0f, playerPos.x - objPos.x + settings.fallOffset));
+
+      float newY = objPos.y + (distance * settings.fallSpeed);
+      obj->setPositionY(newY);
     }
   }
 };
+
+$on_mod(Loaded) {
+  settings.enabled = Mod::get()->getSettingValue<bool>("enabled");
+
+  settings.fallSpeed = Mod::get()->getSettingValue<float>("fall-speed");
+  settings.fallOffset = Mod::get()->getSettingValue<float>("fall-offset");
+
+  listenForSettingChanges<bool>("enabled", [](bool value) {
+    settings.enabled = value;
+  });
+
+  listenForSettingChanges<float>("fall-speed", [](float value) {
+    settings.fallSpeed = value;
+  });
+  listenForSettingChanges<float>("fall-offset", [](float value) {
+    settings.fallOffset = value;
+  });
+
+}
