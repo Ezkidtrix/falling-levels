fc130662ca4f1b288a6e526fa83b2294d18dca8e
BuildTools <unconfigured@null.spigotmc.org>
2026-08-06 16:10:56 -0700

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,20 @@ class $modify(GJBaseGameLayer) {
   void update(float dt) {
     GJBaseGameLayer::update(dt);
 
-    
+    CCArray* objects = this->m_objects;
+    if (!objects) return;
+
+    PlayerObject* player = this->m_player1;
+    if (!player) return;
+
+    CCPoint playerPos = player->getPosition();
+
+    for (int i = 0; i < objects->count(); i++) {
+      GameObject* obj = static_cast<GameObject*>(objects->objectAtIndex(i));
+      if (!obj) continue;
+
+      CCPoint objPos = obj->getPosition();
+      
+    }
   }
 };
